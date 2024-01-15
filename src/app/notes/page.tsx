e6be5333bb8f0fe779beb07728c6f1f3b680839e
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-15 21:47:38 +0400

    Clerk Authentication Setup

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: "FlowBrain - Notes",
+};
+
+export default function NotePage() {
+    return (
+        <div>
+            Here will be notes !
+        </div>
+    )
+}
