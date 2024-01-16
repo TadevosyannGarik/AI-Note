e1c83d692f9174e5555017d60738e45e42e4326c
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-16 21:53:46 +0400

    Minor changes

--- src/components/ai-chat-button.tsx
+++ src/components/ai-chat-button.tsx
@@ -4,15 +4,15 @@ import AIChatBox from "@/components/ai-chat-box";
 import { Button } from "@/components/ui/button";
 
 export default function AIChatButton() {
-  const [chatBoxOpen, setChatBoxOpen] = useState(false);
+    const [chatBoxOpen, setChatBoxOpen] = useState(false);
 
-  return (
-    <>
-      <Button onClick={() => setChatBoxOpen(true)}>
-        <Bot size={20} className="mr-2" />
-        AI Chat
-      </Button>
-      <AIChatBox open={chatBoxOpen} onClose={() => setChatBoxOpen(false)} />
-    </>
-  );
+    return (
+        <>
+            <Button onClick={() => setChatBoxOpen(true)}>
+                <Bot size={20} className="mr-2" />
+                AI Chat
+            </Button>
+            <AIChatBox open={chatBoxOpen} onClose={() => setChatBoxOpen(false)} />
+        </>
+    );
 }
