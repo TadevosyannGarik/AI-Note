125617a51d670bdb63bf2e77f875f07f3f1b66e5
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-18 00:39:00 +0400

    Root Page Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
+import Image from 'next/image'
 
 
 export default function Home() {
@@ -12,7 +13,12 @@ export default function Home() {
   if (userId) redirect("/notes");
     return (
         <>
-            <div className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+            <div className='mb-12 sm:mt-40 flex flex-col items-center justify-center text-center'>
+                <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-lg bg-white/50 px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
+                    <p className='text-sm font-semibold text-gray-700'>
+                        Created by Tadevosyan Industries 
+                    </p>
+                </div>
                 <div
                     aria-hidden='true'
                     className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
@@ -37,14 +43,89 @@ export default function Home() {
                     An intelligent note-taking app with AI integration, built with OpenAI,
                     Pinecone, Next.js, Shadcn UI, Clerk, and more.
                 </p>
-
                 <Button size="lg" asChild className='mt-5'>
                     <Link href="/notes">
                         Get started{' '}
                         <ArrowRight className='ml-2 h-5 w-5' />
                     </Link>
                 </Button>
-            </div> 
+                <div>
+                    <div className='relative isolate'>
+                        <div>
+                            <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+                                <div className='mt-16 flow-root sm:mt-24'>
+                                    <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                                        <Image
+                                            src='/note-ai.png'
+                                            alt='product preview'
+                                            width={1364}
+                                            height={866}
+                                            quality={100}
+                                            className='rounded-md bg-white/55 p-2 sm:p-8 md:p-0 shadow-2xl ring-1 ring-gray-900/10'
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
+                    <div className='mb-12 px-6 lg:px-8'>
+                        <div className='mx-auto max-w-2xl sm:text-center'>
+                            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+                                Start <span className='text-indigo-500'>chatting</span> in minutes
+                            </h2>
+                            <p className='mt-4 text-lg text-gray-600'>
+                                Chatting to your notes has never been
+                                easier than with Note AI.
+                            </p>
+                        </div>
+                    </div>
+                    <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+                        <li className='md:flex-1'>
+                            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+                                <span className='text-sm font-medium text-indigo-500'>
+                                    Step 1
+                                </span>
+                                <span className='text-xl font-semibold'>
+                                        Sign up for an account
+                                </span>
+                                <span className='mt-2 text-zinc-700'>
+                                    Create your account for free. Use email or Providers
+                                </span>
+                            </div>
+                        </li>
+                        <li className='md:flex-1'>
+                            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+                                <span className='text-sm font-medium text-indigo-500'>
+                                    Step 2
+                                </span>
+                                <span className='text-xl font-semibold'>
+                                    Create your Notes
+                                    </span>
+                                <span className='mt-2 text-zinc-700'>
+                                    We&apos;ll process your note and make it
+                                    ready for you to chat with.
+                                </span>
+                            </div>
+                        </li>
+                        <li className='md:flex-1'>
+                            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+                                <span className='text-sm font-medium text-indigo-500'>
+                                    Step 3
+                                </span>
+                                <span className='text-xl font-semibold'>
+                                    Start asking questions
+                                </span>
+                                <span className='mt-2 text-zinc-700'>
+                                    It&apos;s that simple. Try out Note AI today -
+                                    it really takes less than a minute.
+                                </span>
+                            </div>
+                        </li>
+                    </ol>
+                </div>
+            </div>
         </>
     )
 }
